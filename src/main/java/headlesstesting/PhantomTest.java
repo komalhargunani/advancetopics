656084c5dd3951fd9a273f4145c5781353ed8bc5
komalh <komalh@ULTP_649.synerzipune.local>
2017-08-01 14:56:05 +0530

    update

--- src/main/java/headlesstesting/PhantomTest.java
+++ src/main/java/headlesstesting/PhantomTest.java
@@ -6,9 +6,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.testng.annotations.Test;
 
 public class PhantomTest {
-	public static void main(String[] args) {
+	
+	@Test
+	public void testPhantomDemo() {
         File file = new File("C://Program Files//phantomjs-2.1.1-windows//bin//phantomjs.exe");				
         System.setProperty("phantomjs.binary.path", file.getAbsolutePath());		
         WebDriver driver = new PhantomJSDriver();	
