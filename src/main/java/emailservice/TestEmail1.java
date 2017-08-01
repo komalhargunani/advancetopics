1ff59a8a416f54e08adf997d03538565cbdecc85
komalh <komalh@ULTP_649.synerzipune.local>
2017-08-01 14:22:01 +0530

    add

---
+++ src/main/java/emailservice/TestEmail1.java
@@ -0,0 +1,45 @@
+package emailservice;
+
+
+	import org.testng.Assert;
+	import org.testng.annotations.Test;
+
+	import com.relevantcodes.extentreports.ExtentReports;
+	import com.relevantcodes.extentreports.ExtentTest;
+	import com.relevantcodes.extentreports.LogStatus;
+
+	public class TestEmail1 {
+
+		ExtentReports report;
+		ExtentTest logger;
+		
+		@Test
+		public void verifysum(){
+			
+			//Create object for Report with filepath
+			report=new ExtentReports("E:\\synerzip_workspace\\service\\ExternalReport\\TestReport.html");
+			
+			//Start the test
+			logger=report.startTest("VerifySum");
+		
+			//Log the status in report
+			logger.log(LogStatus.INFO, "calc started ");
+		
+			int a=50,b=80,c;
+			c=a+b;
+			Assert.assertEquals(c, 130);
+			
+			//Pass the test in report
+			logger.log(LogStatus.PASS, "Test Verified");
+			
+			//End the test
+			report.endTest(logger);
+			
+			//Flush the data to report
+			report.flush();
+			}
+
+	}
+
+
+
